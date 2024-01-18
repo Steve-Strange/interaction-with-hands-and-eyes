83203fa61e2f79dd3f3e03348954af1533bef7fa
monkey2627 <136638076+monkey2627@users.noreply.github.com>
2024-01-18 23:46:09 +0800

    add_pinch

---
+++ Assets/Scripts/pinch.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pinch : MonoBehaviour
+{
+    public TMPro.TMP_Text T;
+    public GameObject thumb;
+    public GameObject index;
+    public bool ispinch = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void test()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float f = (thumb.transform.position - index.transform.position).magnitude;
+        if (f < 0.01)
+            ispinch = true;
+        else
+            ispinch = false;
+            T.text =ispinch.ToString();
+        
+    }
+}
