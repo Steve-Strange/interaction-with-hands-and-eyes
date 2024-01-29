e532ad300d978bb8bf42d7028e139a65db9404c5
monkey2627 <136638076+monkey2627@users.noreply.github.com>
2024-01-29 23:25:36 +0800

    add_frame

--- Assets/Scripts/collide.cs
+++ Assets/Scripts/collide.cs
@@ -6,33 +6,69 @@ public class collide : MonoBehaviour
 {
     public TMPro.TMP_Text t;
     public GameObject pinch;
+    public GameObject frame;
     private pinch p;
+    public string Frame;//the mark of frame
     public GameObject FinalObjects;
-    public List<GameObject> finalObj;
+    public List<GameObject> finalObj;//get all objects from last level
+    
+    public List<GameObject> anchor;//all anchor object,hilight them,we can only contact with them
+
+    public GameObject[] onFrame;//object already on now frame
     // Start is called before the first frame update
-    private void OnCollisionEnter(Collision collision)
-    {
+
+    private List<GameObject> rectCorner;// leftup rightup rightdown leftdown
+    private Vector3[] rect;
+    private void OnCollisionEnter(Collision collision){
 
         ContactPoint contact = collision.contacts[0];                                                                                                                                                      
         t.text = collision.gameObject.name;
         
-        if(p.ispinch)//ÐÞ¸Ä£¬ÈçºÎ¸üºÃÔÚÏßÉÏ
+        if(p.ispinch)//keep pinch,keep manipulate,todo make p more wending
         { 
           finalObj[0].transform.position = contact.point;
+          finalObj[0].transform.parent = collision.gameObject.transform;
+          
+          if(frame.GetComponent<frame>().frame == 'rect'){
+            rect = frame.GetComponent<frame>().rectCorner;
+            for(int i = 0 ;i <= 3 ;i++)
+            if((finalObj[0].transform.position-rect[i]).magnitude < 0.01)
+            {
+                finalObj[0].transform.position = rect[i];
+                rectCorner[i] = finalObj[0];
+            }
+          }
+          
+          
+          onFrame.add(finalObj[0]);
           finalObj.RemoveAt(0);
         }
-
-           
-
-       
-
     }
     void Start()
     {
         p = pinch.GetComponent<pinch>();
         finalObj =  FinalObjects.GetComponent<FinalObjects >().finalObj;
     }
+    void anchorChoose()
+    {
+        anchor.clear();
+        if(Frame == 'rect'){
+        // select three object by distance ï¼Œadd position correct in the cornerï¼ˆmake object right at the cornerï¼‰
+        if(rectCorner[0] && rectCorner[2])
+        {
+          anchor.add(rectCorner[0]);
+          anchor.add(rectCorner[2]);
+        }
+        else if(rectCorner[1] && rectCorner[3]){
+          anchor.add(rectCorner[1]);
+          anchor.add(rectCorner[3]);
+        }
+        }
 
+        foreach i in anchor{
+            //é«˜äº®
+        }
+    }
     // Update is called once per frame
     void Update()
     {
--- Assets/Scripts/frame.cs
+++ Assets/Scripts/frame.cs
@@ -1,31 +1,38 @@
+using System.Numerics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 public class frame : MonoBehaviour
 {
-    // Start is called before the first frame update
     private LineRenderer line;
+
     private Vector3 forward;//ʵ�ֳ���
     private Vector3 right;
     private Vector3 up;
-    public GameObject head;
     public float dis = 0.01f;
-    private Mesh mesh;
-    public float lenth;
-    public float width;
+    public GameObject head;
+    
+    //rect
+    public float rectlenth = 1f;
+    public float rectheight= 1f;
+    public Vector3[] rectCorner;// save the for corner
+    //circle
     public float height;
     public Camera camera;
     public float R = 0.1f;
     public int N = 40;
-    public GameObject FinalObjects;
+
+    
+    public GameObject[] collider;//存添加的所有collider物体；
 
     Vector3[] points = new Vector3[20];
     Vector3 center;
     
-    public void creatRect()//2d
+    public void creatRect()//2d, just make the origin frame
     {
    
+        frame = 'rect';
         points = new Vector3[4];
 
         forward = head.transform.forward.normalized;
@@ -34,21 +41,65 @@ public class frame : MonoBehaviour
 
         center = head.transform.position + forward * dis;
 
-        line.positionCount=4;
-        
-        points[0]=(new Vector3(0, 0, 0));
-        points[1] = (new Vector3(0, 1, 0));
-        points[2]=(new Vector3(1, 1, 0));
-        points[3]=(new Vector3(1, 0, 0));
-        line.SetPosition(0, new Vector3(0, 0, 0));
-        line.SetPosition(1, new Vector3(0, 1, 0));
-        line.SetPosition(2, new Vector3(1, 1, 0));
-        line.SetPosition(3, new Vector3(1, 0, 0));
+        line.positionCount = 4;
+        rectCorner[0] = center + up/2*rectheight-right/2*rectlenth;
+        rectCorner[1] = center + up/2*rectheight+right/2*rectlenth;
+        rectCorner[2] = center - up/2*rectheight-right/2*rectlenth;
+        rectCorner[3] = center - up/2*rectheight+right/2*rectlenth;
+        for(int i = 0;i<=3;i++){
+            line.SetPosition(i,rectCorner[i]);
+            if(i!=3){
+                addColliderToLine(rectCorner[i],rectCorner[i+1]);
+            }
+            else{
+                addColliderToLine(rectCorner[3],rectCorner[0]);
+            }
+        }
+    }
+    //距离等比例变化
+    public void redoRect(string type,)//visualize the frame by object position
+    {
+        // get the original plane by previous points
+        Vector3 f = ((rectCorner[0]-rectCorner[1])).normalized;
+        Vector3 m = ((rectCorner[0]-rectCorner[3])).normalized;
+        if(type = "1")// 左上，右下
+        {   rectCorner[0] = anchor[0];
+            rectCorner[2] = anchor[1];
+            Vector3 temp = rectCorner[0]-rectCorner[2];
+            rectheight = temp * m;
+            rectlenth = temp * f;
+            rectCorner[1] = rectCorner[0] - f * rectlenth;
+            rectCorner[2] = rectCorner[0] - m * rectheight;
+            
+            for(int i = 0;i<=3;i++){
+            line.SetPosition(i,rectCorner[i]);
+            if(i!=3){
+                resizeColliderToline(collider[i],rectCorner[i],rectCorner[i+1]);
+            }
+            else{
+                resizeColliderToline(collider[i],rectCorner[3],rectCorner[0]);
+            }
+        }
+            
 
-        addColliderToLine(new Vector3(0, 0, 0), new Vector3(0, 1, 0));
-            addColliderToLine(new Vector3(0, 1, 0), new Vector3(1, 1, 0));
-            addColliderToLine(new Vector3(1, 1, 0), new Vector3(1, 0, 0));
-        addColliderToLine(new Vector3(1, 0, 0), new Vector3(0, 0, 0));
+        }
+
+    }
+    private void resizeColliderToline(GameObject col, Vector3 startPos,Vector3 endPos)
+    {
+        float lineLength = Vector3.Distance(startPos, endPos); // length of line
+        col.size = new Vector3(lineLength, 0.0001f, 0.0001f); // size of collider is set where X is length of line, Y is width of line, Z will be set as per requirement
+        Vector3 midPoint = (startPos + endPos) / 2;
+        col.transform.position = midPoint; // setting position of collider object
+        // Following lines calculate the angle between startPos and endPos
+        float angle = (Mathf.Abs(startPos.y - endPos.y) / Mathf.Abs(startPos.x - endPos.x));
+        if ((startPos.y < endPos.y && startPos.x > endPos.x) || (endPos.y < startPos.y && endPos.x > startPos.x))
+        {
+            angle *= -1;
+        }
+        angle = Mathf.Rad2Deg * Mathf.Atan(angle);
+        col.transform.Rotate(0, 0, angle);
+
     }
     private void addColliderToLine(Vector3 startPos,Vector3 endPos)
     {
