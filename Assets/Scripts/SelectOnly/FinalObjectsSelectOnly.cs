5a62faaa6147580f8997fda4c8eaeea7dfa0e04e
Sorcerer Inferior <2694630726@qq.com>
2024-03-25 21:19:50 +0800

    update scale

--- Assets/Scripts/SelectOnly/FinalObjectsSelectOnly.cs
+++ Assets/Scripts/SelectOnly/FinalObjectsSelectOnly.cs
@@ -62,12 +62,18 @@ public class FinalObjectsSelectOnly : MonoBehaviour
         obj.tag = "FinalObject";
         finalObjQ[obj] = obj.transform.rotation;
         obj.GetComponent<Outline>().OutlineColor = Color.clear;
+        
+        float objMaxScale = Mathf.Max(obj.transform.GetComponent<Renderer>().bounds.size.x, obj.transform.GetComponent<Renderer>().bounds.size.y, obj.transform.GetComponent<Renderer>().bounds.size.z);
+        float finalScale = 0.05f / objMaxScale;
+
         obj.transform.parent = transform;
         obj.transform.localEulerAngles = new Vector3(0, 0, 0);
-        obj.transform.localScale = new Vector3(finalScale / transform.localScale.x, finalScale / transform.localScale.y, finalScale / transform.localScale.z);
-        obj.transform.localPosition = new Vector3(obj.transform.localScale.x * 2 * finalObj.Count - transform.localScale.x / 2, 0, -2 * obj.transform.localScale.z);
-        if (SightCone.GetComponent<SightConeSelectOnly>().selectedObjects.Contains(obj)) SightCone.GetComponent<SightConeSelectOnly>().selectedObjects.Remove(obj);
-        SightCone.GetComponent<SightConeSelectOnly>().objectWeights[obj] = -1;
+
+        // obj.transform.localScale = new Vector3(0.1f * obj.transform.localScale.x, 0.1f * obj.transform.localScale.y, 0.01f * obj.transform.localScale.z) / objMaxScale;
+        obj.transform.localScale = new Vector3(obj.transform.localScale.x, obj.transform.localScale.y, obj.transform.localScale.z) * finalScale;
+        obj.transform.localPosition = new Vector3(0.1f * finalObj.Count - 0.35f , 0, - obj.transform.localScale.z);
+        if(SightCone.GetComponent<SightCone>().selectedObjects.Contains(obj)) SightCone.GetComponent<SightCone>().selectedObjects.Remove(obj);
+        SightCone.GetComponent<SightCone>().objectWeights[obj] = -1;
 
         finalObj.Add(obj);
     }
--- Assets/Scripts/SelectOnly/HandPoseManagerSelectOnly.cs
+++ Assets/Scripts/SelectOnly/HandPoseManagerSelectOnly.cs
@@ -135,8 +135,10 @@ public class HandPoseManagerSelectOnly : MonoBehaviour
                 originalTransform[obj.Key] = new TransformData(obj.Key.transform.position, obj.Key.transform.rotation, obj.Key.transform.localScale);
                 obj.Key.GetComponent<Outline>().OutlineColor = Color.clear;
                 objScale[obj.Key] = obj.Key.transform.localScale;
+
                 float objMaxScale = Mathf.Max(obj.Key.transform.GetComponent<Renderer>().bounds.size.x, obj.Key.transform.GetComponent<Renderer>().bounds.size.y, obj.Key.transform.GetComponent<Renderer>().bounds.size.z);
-                obj.Key.transform.localScale = new Vector3(0.1f * obj.Key.transform.GetComponent<Renderer>().bounds.size.x, 0.1f * obj.Key.transform.GetComponent<Renderer>().bounds.size.y, obj.Key.transform.GetComponent<Renderer>().bounds.size.z * 0.1f) / objMaxScale;
+                
+                obj.Key.transform.localScale = new Vector3(0.1f * obj.Key.transform.localScale.x, 0.1f * obj.Key.transform.localScale.y, 0.1f * obj.Key.transform.localScale.z) / objMaxScale;
                 obj.Key.transform.localEulerAngles = new Vector3(0, 0, 0);
                 obj.Key.transform.position = SecondSelectionBG.transform.position +
                     new Vector3(-SecondSelectionBG.transform.localScale.z / 2, -SecondSelectionBG.transform.localScale.y / 2, -SecondSelectionBG.transform.localScale.x / 2) +
